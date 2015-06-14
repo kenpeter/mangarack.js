5ead4b85da95a92d00d295927226c9bba02caa98
kenpeter <figo2476@hotmail.com>
2015-06-14 16:56:39 +1000

    add debug

---
+++ bin/my_mangarack.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+node ./mangarack --output /media/kenpeter/3E3C68780C3F137A/dell_backup_07022012/entertainment/manga -c 6 http://mangafox.me/manga/naruto_gaiden_the_seventh_hokage/
+
+
+#node debug ./mangarack --debug --output /media/kenpeter/3E3C68780C3F137A/dell_backup_07022012/entertainment/manga -c 51 http://mangafox.me/manga/aoi_hana/
