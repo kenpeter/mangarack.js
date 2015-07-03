994797b13b92c7ca12c5611d3f1e01d17a97e3e1
kenpeter <figo2476@hotmail.com>
2015-07-03 17:09:47 +1000

    update

--- bin/my_mangarack.sh
+++ bin/my_mangarack.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-node ./mangarack --output /media/kenpeter/3E3C68780C3F137A/dell_backup_07022012/entertainment/manga -c 6 http://mangafox.me/manga/naruto_gaiden_the_seventh_hokage/
+# It -c means filter great than 9
+#node debug ./mangarack --debug --output /media/kenpeter/3E3C68780C3F137A/dell_backup_07022012/entertainment/manga -c 10 http://mangafox.me/manga/naruto_gaiden_the_seventh_hokage/
 
 
-#node debug ./mangarack --debug --output /media/kenpeter/3E3C68780C3F137A/dell_backup_07022012/entertainment/manga -c 51 http://mangafox.me/manga/aoi_hana/
+node ./mangarack --output /media/kenpeter/3E3C68780C3F137A/dell_backup_07022012/entertainment/manga -c 9.1 http://mangafox.me/manga/naruto_gaiden_the_seventh_hokage/
+
+
+#node ./mangarack --output /media/kenpeter/3E3C68780C3F137A/dell_backup_07022012/entertainment/manga http://bato.to/comic/_/comics/hare-kon-r13233 
+
+
+#node ./mangarack  --output /media/kenpeter/3E3C68780C3F137A/dell_backup_07022012/entertainment/manga http://mangafox.me/manga/boku_wa_mari_no_naka/ -c 52 
+
+#node ./mangarack  --output /media/kenpeter/3E3C68780C3F137A/dell_backup_07022012/entertainment/manga http://bato.to/comic/_/comics/gui-wang-r11551
+
+#node ./mangarack  --output /media/kenpeter/3E3C68780C3F137A/dell_backup_07022012/entertainment/manga http://bato.to/comic/_/comics/undercurrent-r1260
+
+#node ./mangarack  --output /media/kenpeter/3E3C68780C3F137A/dell_backup_07022012/entertainment/manga http://bato.to/comic/_/comics/cage-of-eden-r34
