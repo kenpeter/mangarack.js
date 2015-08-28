fa31b713669a91c1e65d268cb50299c85bb8fec6
kenpeter <figo2476@hotmail.com>
2015-08-29 08:17:00 +1000

    update

--- bin/my_mangarack.sh
+++ bin/my_mangarack.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # It -c means filter great than 9
-#node debug ./mangarack --debug --output /media/kenpeter/3E3C68780C3F137A/dell_backup_07022012/entertainment/manga -c 10 http://mangafox.me/manga/naruto_gaiden_the_seventh_hokage/
+node debug ./mangarack --debug --output /media/kenpeter/3E3C68780C3F137A/dell_backup_07022012/entertainment/manga -c 10 http://mangafox.me/manga/naruto_gaiden_the_seventh_hokage/
 
 
-node ./mangarack --output /media/kenpeter/3E3C68780C3F137A/dell_backup_07022012/entertainment/manga -c 9.1 http://mangafox.me/manga/naruto_gaiden_the_seventh_hokage/
+#node ./mangarack --output /media/kenpeter/3E3C68780C3F137A/dell_backup_07022012/entertainment/manga -c 10 http://mangafox.me/manga/naruto_gaiden_the_seventh_hokage/
 
 
 #node ./mangarack --output /media/kenpeter/3E3C68780C3F137A/dell_backup_07022012/entertainment/manga http://bato.to/comic/_/comics/hare-kon-r13233 
